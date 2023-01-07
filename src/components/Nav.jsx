9ddeb99cbaceadd952644a8688e0fd28d646b948
shypang <smartdragon417@gmail.com>
2023-01-07 22:35:27 +0900

    editing Nav.css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,30 +6,35 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState(1);
   return (
     <nav className="wrapper">
-      <Link to="/swap" className="nav-link" onClick={() => setActiveNav(1)}>
+      <Link to="/home" className="nav-link" onClick={() => setActiveNav(1)}>
         <div className={activeNav === 1 ? "nav-item active" : "nav-item"}>
-          swap
+          홈
+        </div>
+      </Link>
+      <Link to="/swap" className="nav-link" onClick={() => setActiveNav(1)}>
+        <div className={activeNav === 2 ? "nav-item active" : "nav-item"}>
+          스왑
         </div>
       </Link>
 
       <Link to="/token" className="nav-link" onClick={() => setActiveNav(2)}>
         {" "}
-        <div className={activeNav === 2 ? "nav-item active" : "nav-item"}>
-          token
+        <div className={activeNav === 3 ? "nav-item active" : "nav-item"}>
+          토큰
         </div>
       </Link>
 
       <Link to="/nft" className="nav-link" onClick={() => setActiveNav(3)}>
         {" "}
-        <div className={activeNav === 3 ? "nav-item active" : "nav-item"}>
-          nft
+        <div className={activeNav === 4 ? "nav-item active" : "nav-item"}>
+          NFT
         </div>
       </Link>
 
       <Link to="/pools" className="nav-link" onClick={() => setActiveNav(4)}>
         {" "}
-        <div className={activeNav === 4 ? "nav-item active" : "nav-item"}>
-          pools
+        <div className={activeNav === 5 ? "nav-item active" : "nav-item"}>
+          POOL
         </div>
       </Link>
     </nav>
