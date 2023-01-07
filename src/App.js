03af6f1f370f6f203590e60d6bcb432e8a127b58
shypang <smartdragon417@gmail.com>
2023-01-07 22:27:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import swap from "./pages/swap";
+import nft from "./pages/nft";
+import token from "./pages/token";
+import pools from "./pages/pools";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/swap" component={swap} />
+        <Route path="/token" component={token} />
+        <Route path="/nft" component={nft} />
+        <Route path="/pools" component={pools} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+//import ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import Nav from "./components/Nav";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+//import reportWebVitals from "./reportWebVitals";
+//import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Nav />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+//reportWebVitals();
+// ReactDOM.render( // 18버전부터 ReactDom.render 안 씀
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Nav />
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
